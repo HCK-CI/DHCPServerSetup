6641ac7d1445be20422a28f2acc66fc3a0d61a60
Kostiantyn Kostiuk <konstantin@daynix.com>
2022-05-12 12:03:52 +0000

    test: Always pull a docker image
    Arch Linux is a rolling release distribution. If the image
    is old, then some packages cannot be installed due to old
    PGP keys.
    
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- test/build-img.sh
+++ test/build-img.sh
@@ -7,4 +7,5 @@ work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker run \
     --rm \
     --volume "${work_dir}":/data:Z \
+    --pull always \
     archlinux bash /data/build-internal.sh
