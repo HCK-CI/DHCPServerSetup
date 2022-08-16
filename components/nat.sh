36b62e867b4cdd966149680d4b3d9487eb3b7033
Akihiko Odaki <akihiko.odaki@daynix.com>
2022-08-16 21:05:35 +0900

    Use firewalld or ufw instead of sysctl and iptables
    Usually firewalld rules net.ipv4.ip_forward sysctl and netfilter on
    Fedora and ufw does so on Ubuntu. They can also persist the
    configuration as the other part of this setup does and unlike iptables.
    
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- components/nat.sh
+++ components/nat.sh
@@ -5,22 +5,31 @@ set -e
 configure_nat() {
   log_info "Configuring NAT"
 
-  log_info "Enabling IP forwarding"
-  echo 'net.ipv4.ip_forward=1' > /etc/sysctl.conf
-  sysctl -p /etc/sysctl.conf
+  if command_exists firewall-cmd; then
+    firewall-cmd -q --add-forward --permanent
+    firewall-cmd -q --add-masquerade --permanent
+    firewall-cmd -q --reload
+  elif command_exists ufw; then
+    switches=( $(ip route ls | \
+      awk '/^default / {
+            for(i=0;i<NF;i++) { if ($i == "dev") { print $(i+1); next; } }
+           }'
+          ) )
 
-  log_info "Enabling NAT masquerade"
-  switches=( $(ip route ls | \
-    awk '/^default / {
-          for(i=0;i<NF;i++) { if ($i == "dev") { print $(i+1); next; } }
-         }'
-        ) )
+    switch="${switches[0]}"
+    log_info "Selected external interface: ${switch}"
 
-  switch="${switches[0]}"
-  log_info "Selected external interface: ${switch}"
+    echo net/ipv4/ip_forward=1 >> /etc/ufw/sysctl.conf
+    cat >> /etc/ufw/before.rules <<EOF
+*nat
+-A POSTROUTING -o "${switch}" -j MASQUERADE
+COMMIT
+EOF
 
-  iptables -t nat -A POSTROUTING -o "${switch}" -j MASQUERADE
-
-  iptables -A FORWARD -i "${bridge}" -j ACCEPT
-  iptables -A FORWARD -o "${bridge}" -j ACCEPT
+    ufw route allow in on "${bridge}"
+    ufw route allow out on "${bridge}"
+    ufw reload
+  else
+    log_fatal "No supported netfilter frontend found."
+  fi
 }
