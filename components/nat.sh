568eeb8f0c874f3abd581c821a0d0b62e4a7c69f
Akihiko Odaki <akihiko.odaki@daynix.com>
2022-08-16 21:05:35 +0900

    Use firewalld instead of sysctl and iptables
    Unlike iptables, firewalld can persist the configuration as the
    other part of this setup does.
    
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- components/nat.sh
+++ components/nat.sh
@@ -5,22 +5,7 @@ set -e
 configure_nat() {
   log_info "Configuring NAT"
 
-  log_info "Enabling IP forwarding"
-  echo 'net.ipv4.ip_forward=1' > /etc/sysctl.conf
-  sysctl -p /etc/sysctl.conf
-
-  log_info "Enabling NAT masquerade"
-  switches=( $(ip route ls | \
-    awk '/^default / {
-          for(i=0;i<NF;i++) { if ($i == "dev") { print $(i+1); next; } }
-         }'
-        ) )
-
-  switch="${switches[0]}"
-  log_info "Selected external interface: ${switch}"
-
-  iptables -t nat -A POSTROUTING -o "${switch}" -j MASQUERADE
-
-  iptables -A FORWARD -i "${bridge}" -j ACCEPT
-  iptables -A FORWARD -o "${bridge}" -j ACCEPT
+  firewall-cmd -q --add-forward --permanent
+  firewall-cmd -q --add-masquerade --permanent
+  firewall-cmd -q --reload
 }
